c3fbfc534ed255838f766aa9062ccb1ae0c0e23c
zongchang <zongchang@zju.edu.cn>
2020-06-10 08:01:45 +0800

    kg link for question parser

---
+++ openks/app/embedder.py
@@ -0,0 +1,11 @@
+"""
+This module implements an embedder for encoding entities and relations from symbolic space to vector space using existing graph embedding models
+"""
+import 
+
+class SimpleEmbedder(object):
+
+	def __init__(self, gr_name):
+		self.gr_name = gr_name
+
+	def 
--- openks/app/qa/question_parser.py
+++ openks/app/qa/question_parser.py
@@ -3,15 +3,25 @@ This module implements a basic parsing procedure for recoginzing entities and re
 """
 import
 
+class StrucQuestion(object):
+	pass
+	
+
 class QuestionParser(object):
 	
-	def __init__(self):
-		pass
+	def __init__(self, kg_name: str):
+		self.kg_name = kg_name
 
-	def parse_entity(self, question: str) -> list:
+	def parse_entity(self, question: str) -> dict:
+		"""
+		tag: entity, name: xxx, type: xxx
+		"""
 		raise NotImplemented
 
-	def parse_relation(self, question: str) -> list:
+	def parse_relation(self, question: str) -> dict:
+		"""
+		tag: relation, type: xxx
+		"""
 		raise NotImplemented
 
 	def parse(self, question: str) -> dict:
@@ -19,6 +29,22 @@ class QuestionParser(object):
 		relation_list = self.parse_relation(question)
 		return {'ent': entity_list, 'rel': relation_list}
 
+	def kg_link(self, parse_res: dict) -> dict:
+		linked_res = {'items': []}
+		if parse_res['tag'] == 'entity':
+			for item in parse_res['items']:
+				item["id"] = kg_store[self.kg_name].search(mode='entity', name=item['name'], type=item['type'])
+				linked_res['items'].append(item)
+			linked_res['tag'] = 'entity'
+		elif parse_res['tag'] == 'relation':
+			for item in parse_res['items']:
+				item["id"] = kg_store[self.kg_name].search(mode='relation', type=item['type'])
+				linked_res['items'].append(item)
+			linked_res['tag'] = 'relation'
+		return linked_res
+
 if __name__ == '__main__':
 	parser = QuestionParser()
-	print(parser.parse()) 
+	parse_res = parser.parse()
+	print(parser_res) 
+	print(parser.kg_link(parser_res))
