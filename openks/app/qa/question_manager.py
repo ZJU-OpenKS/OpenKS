161de51196126d92bdc0ee463b1f023a7e592ef7
zongchang <zongchang@zju.edu.cn>
2020-06-11 14:15:34 +0800

    construct entity and relation class, removed embedder

--- openks/app/qa/question_manager.py
+++ openks/app/qa/question_manager.py
@@ -1,6 +1,80 @@
 """
 This module implements a basic parsing procedure for recoginzing entities and relation types from questions
 """
+import paddle.fluid as fluid
+
+class EntityObj(object):
+
+	def __init__(self):
+		self.__name = ''
+		self.__ent_type = ''
+		self.__ent_id = ''
+		self.__embedding = fluid.data(name='entity', shape=[1, None], dtype='float32')
+
+	@property
+	def name(self):
+		return self.__name
+
+	@name.setter
+	def name(self, name):
+		self.__name = name
+
+	@property
+	def ent_type(self):
+		return self._ent_type
+	
+	@ent_type.setter
+	def ent_type(self, ent_type):
+		self.__ent_type = ent_type
+
+	@property
+	def ent_id(self):
+		return self.__ent_id
+
+	@ent_id.setter
+	def ent_id(self, ent_id):
+		self.__ent_id = ent_id
+
+	@property
+	def embedding(self):
+		return self.__embedding
+	
+	@embedding.setter
+	def embedding(self, embedding):
+		self.__embedding = embedding
+
+class RelationObj(object):
+
+	def __init__(self):
+		self.__rel_type = ''
+		self.__rel_id = ''
+		self.__embedding = fluid.data(name='relation', shape=[1, None], dtype='float32')
+		# rel_id can be relation type id or specific relation id, depends on whether to distinguish relations among different entity pairs
+
+	@property
+	def rel_type(self):
+		return self._rel_type
+	
+	@rel_type.setter
+	def rel_type(self, rel_type):
+		self.__rel_type = rel_type
+
+	@property
+	def rel_id(self):
+		return self._rel_id
+	
+	@rel_id.setter
+	def rel_id(self, rel_id):
+		self.__rel_id = rel_id
+
+	@property
+	def embedding(self):
+		return self._embedding
+	
+	@embedding.setter
+	def embedding(self, embedding):
+		self.__embedding = embedding
+		
 
 class StrucQ(object):
 	
@@ -81,6 +155,7 @@ class QuestionManager(object):
 		tag: entity, [name: xxx, type: xxx]
 		"""
 		entities = []
+		entities.append(EntityObj)
 		""" To be implemented """
 		self.struc_q.entities = entities
 
@@ -89,6 +164,7 @@ class QuestionManager(object):
 		tag: relation, [type: xxx]
 		"""
 		relations = []
+		relations.append(RelationObj)
 		""" To be implemented """
 		self.struc_q.relations = relations
 
@@ -106,18 +182,34 @@ class QuestionManager(object):
 		""" To be implemented """
 		self.struc_q.question_class = question_class
 
-	def link_and_embed(self) -> None:
+	def entity_link(self) -> None:
 		"""
-		entity linking and transform entities and relations to embeding representations
+		entity linking
 		"""
 		entities = []
 		for ent in self.struc_q.entities:
 			""" use self.kg_name to link entity and get id """
-			""" use self.kg_name and id to search embedding store and get representation """
-			""" replace entity with a dictionary with its id and representation """
+			""" replace entity with a dictionary with its id """
 			""" To be implemented """
 			entities.append(ent)
 		self.struc_q.entities = entities
+
+	def entity_relation_embed(self) -> None:
+		"""
+		transform entities and relations to embeding representations
+		"""
+		entities = []
+		for ent in self.struc_q.entities:
+			""" use self.kg_name and id to search embedding store and get representation """
+			""" replace entity with a dictionary with its representation """
+			""" To be implemented """
+			entities.append(ent)
+		self.struc_q.entities = entities
+		relations = []
+		for rel in self.struc_q.relations:
+			""" To be implemented """
+			relations.append(rel)
+		self.struc_q.relations = relations
 
 	def simple_parser(self) -> StrucQ:
 		"""
@@ -126,7 +218,8 @@ class QuestionManager(object):
 		self.sentence_parse()
 		self.entity_extract()
 		self.relation_extract()
-		self.link_and_embed()
+		self.entity_link()
+		self.entity_relation_embed()
 		return self.struc_q
 
 
@@ -135,3 +228,4 @@ if __name__ == '__main__':
 	res = parser.simple_parser()
 	print(res.text)
 	print(res.entities)
+	print(res.relations)
