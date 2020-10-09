e9644f6805f3369638fd12c261f01d21fa3ffa3b
zongchang <zongchang@zju.edu.cn>
2020-10-09 08:26:07 +0800

    updated models package names

--- openks/models/pytorch/__init__.py
+++ openks/models/pytorch/__init__.py
@@ -1,7 +1,7 @@
 """
 init
 """
-from .kglearn import *
-from .general import *
+from .kg_learn import *
+from .gen_learn import *
 from .kg_modules import *
-from .modules import *
+from .gen_modules import *
