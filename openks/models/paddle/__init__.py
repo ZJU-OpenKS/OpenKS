136f0a3e793be5a5ed633a8128292a9173a5f73d
zhengkangjie <kangjie.zheng@gmail.com>
2021-01-29 11:14:07 +0800

    group_2 gcn and gat

--- openks/models/paddle/__init__.py
+++ openks/models/paddle/__init__.py
@@ -4,7 +4,7 @@
 """
 init
 """
-from .kg_learn import *
-from .gen_learn import *
-from .kg_modules import *
-from .gen_modules import *
+# from .kg_learn import *
+# from .gen_learn import *
+# from .kg_modules import *
+# from .gen_modules import *
