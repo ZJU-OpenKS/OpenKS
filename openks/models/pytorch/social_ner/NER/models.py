5ceafeb1a81f2c84488d000f27be759090bd596d
Gezelligheid520 <15201310296@163.com>
2023-02-13 22:18:38 +0800

    add socail ner modules

---
+++ openks/models/pytorch/social_ner/NER/models.py
@@ -0,0 +1,56 @@
+import torch
+import torch.functional as F
+import torch.nn as nn
+from pytorch_transformers import BertModel, BertPreTrainedModel
+from torchcrf import CRF
+
+
+class BERT_BiLSTM_CRF(BertPreTrainedModel):
+    def __init__(self, config, need_birnn=False, rnn_dim=128):
+        super(BERT_BiLSTM_CRF, self).__init__(config)
+
+        self.num_tags = config.num_labels
+        self.bert = BertModel(config)
+        self.dropout = nn.Dropout(config.hidden_dropout_prob)
+        out_dim = config.hidden_size
+        self.need_birnn = need_birnn
+
+        # 如果为False，则不要BiLSTM层
+        if need_birnn:
+            self.birnn = nn.LSTM(
+                config.hidden_size,
+                rnn_dim,
+                num_layers=1,
+                bidirectional=True,
+                batch_first=True,
+            )
+            out_dim = rnn_dim * 2
+
+        self.hidden2tag = nn.Linear(out_dim, config.num_labels)
+        self.crf = CRF(config.num_labels, batch_first=True)
+
+    def forward(self, input_ids, tags, token_type_ids=None, input_mask=None):
+        emissions = self.tag_outputs(input_ids, token_type_ids, input_mask)
+        loss = -1 * self.crf(emissions, tags, mask=input_mask.byte())
+
+        return loss
+
+    def tag_outputs(self, input_ids, token_type_ids=None, input_mask=None):
+
+        outputs = self.bert(
+            input_ids, token_type_ids=token_type_ids, attention_mask=input_mask
+        )
+
+        sequence_output = outputs[0]
+
+        if self.need_birnn:
+            sequence_output, _ = self.birnn(sequence_output)
+
+        sequence_output = self.dropout(sequence_output)
+        emissions = self.hidden2tag(sequence_output)
+
+        return emissions
+
+    def predict(self, input_ids, token_type_ids=None, input_mask=None):
+        emissions = self.tag_outputs(input_ids, token_type_ids, input_mask)
+        return self.crf.decode(emissions, input_mask.byte())
---
+++ openks/models/pytorch/social_ner/NER/utils.py
@@ -0,0 +1,246 @@
+import logging
+import os
+import pickle
+import sys
+import jsonlines
+import torch
+from torch.utils.data import TensorDataset
+from tqdm import tqdm
+from collections import defaultdict as ddict
+logger = logging.getLogger(__name__)
+
+
+class InputExample(object):
+    """A single training/test example for simple sequence classification."""
+
+    def __init__(self, guid, text, label=None):
+        self.guid = guid
+        self.text = text
+        self.label = label
+
+
+class InputFeatures(object):
+    """A single set of features of data."""
+
+    def __init__(
+        self, input_ids, input_mask, segment_ids, label_id, ori_tokens
+    ):
+
+        self.input_ids = input_ids
+        self.input_mask = input_mask
+        self.segment_ids = segment_ids
+        self.label_id = label_id
+        self.ori_tokens = ori_tokens
+
+
+class NerProcessor(object):
+    def read_data(self, input_file):
+        """Reads a BIO data."""
+        with open(input_file, "r",errors='ignore') as f:
+            lines = []
+            words = []
+            labels = []
+
+            for line in f.readlines():
+                contends = line.strip()
+                tokens = line.strip().split("\t")
+
+                if len(tokens) == 2:
+                    words.append(tokens[0])
+                    labels.append(tokens[1])
+                else:
+                    if len(contends) == 0 and len(words) > 0:
+                        label = []
+                        word = []
+                        for l, w in zip(labels, words):
+                            if len(l) > 0 and len(w) > 0:
+                                label.append(l)
+                                word.append(w)
+                        lines.append([" ".join(label), " ".join(word)])
+                        words = []
+                        labels = []
+
+            return lines
+
+    def get_labels(self, args):
+
+        new_list=[
+        'B',
+        'I',
+        'O'
+         ]
+        relations = ddict(int)
+        with open('./data/dataset.jsonl', 'r', encoding='utf-8') as fp:
+            for item in jsonlines.Reader(fp):
+                if item["entities"]:
+                    for t in item["entities"]:
+                        relations[t['type']] += 1 
+        for re in relations.keys():
+            new_list.append('B-'+re)
+            new_list.append('I-'+re)
+        return new_list
+
+    def get_examples(self, input_file):
+        examples = []
+
+        lines = self.read_data(input_file)
+
+        for i, line in enumerate(lines):
+            guid = str(i)
+            text = line[1]
+            label = line[0]
+
+            examples.append(InputExample(guid=guid, text=text, label=label))
+
+        return examples
+
+
+def convert_examples_to_features(
+    args, examples, label_list, max_seq_length, tokenizer
+):
+    
+    label_map = {label: i for i, label in enumerate(label_list)}
+
+    features = []
+
+    for (ex_index, example) in tqdm(
+        enumerate(examples), desc="convert examples"
+    ):
+        # if ex_index % 10000 == 0:
+        #     logger.info("Writing example %d of %d" % (ex_index, len(examples)))
+
+        textlist = example.text.split(" ")
+        labellist = example.label.split(" ")
+        assert len(textlist) == len(labellist)
+        tokens = []
+        labels = []
+        ori_tokens = []
+
+        for i, word in enumerate(textlist):
+            # 防止wordPiece情况出现，不过貌似不会
+
+            token = tokenizer.tokenize(word)
+            tokens.extend(token)
+            label_1 = labellist[i]
+            ori_tokens.append(word)
+
+            # 单个字符不会出现wordPiece
+            for m in range(len(token)):
+                if m == 0:
+                    labels.append(label_1)
+                else:
+                    print("出现wordPiece情况\n")
+                    labels.append("O")
+                    # if label_1 == "O":
+                    #     labels.append("O")
+                    # else:
+                    #     labels.append("I")
+
+        if len(tokens) >= max_seq_length - 1:
+            tokens = tokens[0 : (max_seq_length - 2)]  # -2 的原因是因为序列需要加一个句首和句尾标志
+            labels = labels[0 : (max_seq_length - 2)]
+            ori_tokens = ori_tokens[0 : (max_seq_length - 2)]
+
+        ori_tokens = ["[CLS]"] + ori_tokens + ["[SEP]"]
+
+        ntokens = []
+        segment_ids = []
+        label_ids = []
+        ntokens.append("[CLS]")
+        segment_ids.append(0)
+        label_ids.append(label_map["O"])
+
+        for i, token in enumerate(tokens):
+            ntokens.append(token)
+            segment_ids.append(0)
+            label_ids.append(label_map[labels[i]])
+
+        ntokens.append("[SEP]")
+        segment_ids.append(0)
+        label_ids.append(label_map["O"])
+        input_ids = tokenizer.convert_tokens_to_ids(ntokens)
+
+        input_mask = [1] * len(input_ids)
+
+        # assert len(ori_tokens) == len(
+        #     ntokens
+        # ), f"{len(ori_tokens)}, {len(ntokens)}, {ori_tokens},{ntokens}"
+
+        while len(input_ids) < max_seq_length:
+            input_ids.append(0)
+            input_mask.append(0)
+            segment_ids.append(0)
+            # we don't concerned about it!
+            label_ids.append(0)
+            ntokens.append("**NULL**")
+
+        assert len(input_ids) == max_seq_length
+        assert len(input_mask) == max_seq_length
+        assert len(segment_ids) == max_seq_length
+        assert len(label_ids) == max_seq_length
+
+        if ex_index < 5:
+            logger.info("*** Example ***")
+            logger.info("guid: %s" % (example.guid))
+            logger.info("tokens: %s" % " ".join([str(x) for x in ntokens]))
+            logger.info("input_ids: %s" % " ".join([str(x) for x in input_ids]))
+            logger.info(
+                "input_mask: %s" % " ".join([str(x) for x in input_mask])
+            )
+            logger.info(
+                "segment_ids: %s" % " ".join([str(x) for x in segment_ids])
+            )
+            logger.info("label_ids: %s" % " ".join([str(x) for x in label_ids]))
+
+        # if not os.path.exists(os.path.join(output_dir, 'label2id.pkl')):
+        #     with open(os.path.join(output_dir, 'label2id.pkl'), 'wb') as w:
+        #         pickle.dump(label_map, w)
+
+        features.append(
+            InputFeatures(
+                input_ids=input_ids,
+                input_mask=input_mask,
+                segment_ids=segment_ids,
+                label_id=label_ids,
+                ori_tokens=ori_tokens,
+            )
+        )
+
+    return features
+
+
+def get_Dataset(args, processor, tokenizer, mode="train"):
+    if mode == "train":
+        filepath = args.train_file
+    elif mode == "eval":
+        filepath = args.eval_file
+    elif mode == "test":
+        filepath = args.test_file
+    else:
+        raise ValueError("mode must be one of train, eval, or test")
+
+    examples = processor.get_examples(filepath)
+    label_list = args.label_list
+
+    features = convert_examples_to_features(
+        args, examples, label_list, args.max_seq_length, tokenizer
+    )
+
+    all_input_ids = torch.tensor(
+        [f.input_ids for f in features], dtype=torch.long
+    )
+    all_input_mask = torch.tensor(
+        [f.input_mask for f in features], dtype=torch.long
+    )
+    all_segment_ids = torch.tensor(
+        [f.segment_ids for f in features], dtype=torch.long
+    )
+    all_label_ids = torch.tensor(
+        [f.label_id for f in features], dtype=torch.long
+    )
+
+    data = TensorDataset(
+        all_input_ids, all_input_mask, all_segment_ids, all_label_ids
+    )
+
+    return examples, features, data
